ef71c53b658b43ebda39f5dd182815ae09165976
yevheniimoskalenko <testjokerqwerty@gmail.com>
2020-05-04 21:05:01 +0300

    add fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,14 @@ module.exports = {
 				content: process.env.npm_package_description || ''
 			}
 		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{
+				rel: 'stylesheet',
+				href:
+					'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap'
+			}
+		]
 	},
 	/*
 	 ** Customize the progress-bar color
